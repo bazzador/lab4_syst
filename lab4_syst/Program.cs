6908588b502daf5c14442a23401d30b404609976
latysh.ivan1122 <latysh.ivan1122@vu.cdu.edu.ua>
2024-05-25 17:10:52 +0300

    1 процес 4 ресурси

--- lab4_syst/Program.cs
+++ lab4_syst/Program.cs
@@ -14,13 +14,19 @@ namespace lab4_syst
             int bankResource = 10;
             int[] currentUsage = new int[4];
             int[] maxUsage = new int[4];
+            int[] copyMaxUsage = new int[4];
             maxUsage[0] = 6;
             maxUsage[1] = 5;
             maxUsage[2] = 4;
             maxUsage[3] = 7;
-            bool[] isCompleted = new bool[4];
-            while (currentUsage[0] != maxUsage[0] || currentUsage[1] != maxUsage[1] || currentUsage[2] != maxUsage[2] || currentUsage[3] != maxUsage[3])
+            for (int i = 0; i < 4; i++)
             {
+                copyMaxUsage[i] = maxUsage[i];
+            }
+            bool[] isCompleted = new bool[4];
+            while (currentUsage[0] != copyMaxUsage[0] || currentUsage[1] != copyMaxUsage[1] || currentUsage[2] != copyMaxUsage[2] || currentUsage[3] != copyMaxUsage[3])
+            {
+                Console.WriteLine($"bank: {bankResource}");
                 for(int usage = 0; usage < 4; usage++)
                 {
                     if (!isCompleted[usage])
@@ -39,14 +45,15 @@ namespace lab4_syst
                                 {
                                     currentUsage[i] = maxUsage[i];
                                     bankResource = maxUsage[i];
-                                    //maxUsage[i] = 99;
+                                    maxUsage[i] = 99;
+                                    
                                     isCompleted[i] = true;
                                     break;
                                 }
                             }
                         }
                     }
-                    Console.WriteLine($"{currentUsage[usage]} || {maxUsage[usage]}");
+                    Console.WriteLine($"{currentUsage[usage]} || {copyMaxUsage[usage]}");
                 }
                 Console.WriteLine("-----------");
             }
