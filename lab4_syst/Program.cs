852c4950382bd405ee338f0c22132a27453befaa
latysh.ivan1122 <latysh.ivan1122@vu.cdu.edu.ua>
2024-05-25 17:18:09 +0300

    1 процес 10 ресурсів

--- lab4_syst/Program.cs
+++ lab4_syst/Program.cs
@@ -11,35 +11,42 @@ namespace lab4_syst
     {
         static void Main(string[] args)
         {
-            int bankResource = 10;
-            int[] currentUsage = new int[4];
-            int[] maxUsage = new int[4];
-            int[] copyMaxUsage = new int[4];
-            maxUsage[0] = 6;
-            maxUsage[1] = 5;
-            maxUsage[2] = 4;
-            maxUsage[3] = 7;
-            for (int i = 0; i < 4; i++)
+            int bankResource = 15;
+            int[] currentUsage = new int[10];
+            int[] maxUsage = { 6, 5, 4, 4, 7, 9, 4, 7, 2, 5 };
+            int[] copyMaxUsage = new int[10];
+            for (int i = 0; i < 10; i++)
             {
                 copyMaxUsage[i] = maxUsage[i];
             }
-            bool[] isCompleted = new bool[4];
-            while (currentUsage[0] != copyMaxUsage[0] || currentUsage[1] != copyMaxUsage[1] || currentUsage[2] != copyMaxUsage[2] || currentUsage[3] != copyMaxUsage[3])
+            bool[] isCompleted = new bool[10];
+            while (currentUsage[0] != copyMaxUsage[0] || currentUsage[1] != copyMaxUsage[1] || currentUsage[2] != copyMaxUsage[2] || currentUsage[3] != copyMaxUsage[3]
+                || currentUsage[4] != copyMaxUsage[4] || currentUsage[5] != copyMaxUsage[5] || currentUsage[6] != copyMaxUsage[6] || currentUsage[7] != copyMaxUsage[7]
+                || currentUsage[8] != copyMaxUsage[8] || currentUsage[9] != copyMaxUsage[9]) 
             {
+                if(bankResource <= 0)
+                {
+                    Console.WriteLine("BANKROT");
+                    break;
+                }
                 Console.WriteLine($"bank: {bankResource}");
-                for(int usage = 0; usage < 4; usage++)
+                for(int usage = 0; usage < 10; usage++)
                 {
                     if (!isCompleted[usage])
                     {
-                        if ((bankResource + currentUsage[0] > maxUsage[0] || bankResource + currentUsage[1] > maxUsage[1] ||
-                       bankResource + currentUsage[2] > maxUsage[2] || bankResource + currentUsage[3] > maxUsage[3]))
+                        if (bankResource + currentUsage[0] > maxUsage[0] || bankResource + currentUsage[1] > maxUsage[1]
+                       || bankResource + currentUsage[2] > maxUsage[2] || bankResource + currentUsage[3] > maxUsage[3]
+                       || bankResource + currentUsage[4] > maxUsage[4] || bankResource + currentUsage[5] > maxUsage[5]
+                       || bankResource + currentUsage[6] > maxUsage[6] || bankResource + currentUsage[7] > maxUsage[7]
+                       || bankResource + currentUsage[8] > maxUsage[8] || bankResource + currentUsage[9] > maxUsage[9]
+                       )
                         {
                             currentUsage[usage]++;
                             bankResource--;
                         }
                         else
                         {
-                            for (int i = 0; i < 4; i++)
+                            for (int i = 0; i < 10; i++)
                             {
                                 if (currentUsage[i] + bankResource == maxUsage[i])
                                 {
